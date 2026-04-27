a97e0b031f7433691946975599cc3d9a742b5179
bhumirabadiya06 <bhumirabadiya06@gmail.com>
2026-04-27 21:27:22 +0530

    Create  SCS_DP.java

---
+++ SCS_DP.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class SCS_DP {
+    public static int lcs(String s1, String s2) {
+        int m = s1.length();
+        int n = s2.length();
+
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                }
+                else if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
+                else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first string: ");
+        String s1 = sc.nextLine();
+
+        System.out.print("Enter second string: ");
+        String s2 = sc.nextLine();
+
+        int lcsLength = lcs(s1, s2);
+        int scsLength = s1.length() + s2.length() - lcsLength;
+
+        System.out.println("Length of Shortest Common Supersequence = " + scsLength);
+
+        sc.close();
+    }
+}
